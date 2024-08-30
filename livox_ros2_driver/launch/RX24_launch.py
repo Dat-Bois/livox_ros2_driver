a9d3ffa288d60aa524f91cabbf3837706e2e2845
Dat-Bois <eeshvij@gmail.com>
2024-08-30 13:43:35 -0700

    Some minor config changes

--- livox_ros2_driver/launch/RX24_launch.py
+++ livox_ros2_driver/launch/RX24_launch.py
@@ -6,7 +6,7 @@ import launch
 
 ################### user configure parameters for ros2 start ###################
 xfer_format   = 0    # 0-Pointcloud2(sensor_msgs/PointCloud2.msg), 1-customized pointcloud format
-multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
+multi_topic   = 1    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar,1-hub
 publish_freq  = 30.0 # freqency of publish,1.0,2.0,5.0,10.0,etc
 output_type   = 0
